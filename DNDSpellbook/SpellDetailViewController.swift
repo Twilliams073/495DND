648f85e289529b4b8bf1020fde7489bcf94b34a4
Tristan Williams <tristanwilliamsrc@gmail.com>
2023-05-08 14:21:14 -0700

    Adding and Removing Spells

--- DNDSpellbook/SpellDetailViewController.swift
+++ DNDSpellbook/SpellDetailViewController.swift
@@ -17,11 +17,43 @@ class SpellDetailViewController: UIViewController {
     @IBOutlet weak var spellLevel: UILabel!
     @IBOutlet weak var spellCT: UILabel!
     
-    
-    
     var spell: Spell!
     var spellDetailed: SpellDetail?
+    var spellBook: Spellbooks?
+    
+    @IBAction func addToSpellbook(_ sender: Any) {
+        
+        let spls = self.spellBook?.spells
+        let addr = "," + (self.spellDetailed?.index)!
+        spellBook?.spells = (String(spls!) + String(addr))
+        spellBook?.save { [weak self] result in
+            switch result {
+            case .success(let spellbook):
+                print("✅ Spellbook Saved \(spellbook)")
 
+            case .failure(let error):
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
+    }
+    
+    @IBAction func removeFromSpellbook(_ sender: Any) {
+        
+        let spls = self.spellBook?.spells
+        let addr = "," + (self.spellDetailed?.index)!
+        let upd = (spls!).replacingOccurrences(of: addr , with: "")
+        spellBook?.spells = String(upd)
+        spellBook?.save { [weak self] result in
+            switch result {
+            case .success(let spellbook):
+                print("✅ Spellbook Saved \(spellbook)")
+
+            case .failure(let error):
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Gets index to use for spell url from ClassSpellViewController
@@ -32,22 +64,22 @@ class SpellDetailViewController: UIViewController {
             if let error = error {
                 print("❌ Network error: \(error.localizedDescription)")
             }
-
+            
             guard let data = data else {
                 print("❌ Data is nil")
                 return
             }
-
+            
             do {
                 let decoder = JSONDecoder()
-
+                
                 let response = try decoder.decode(SpellDetail.self, from: data)
-
+                
                 let spellDetailed = response
                 print(spellDetailed)
-
+                
                 DispatchQueue.main.async {
-
+                    
                     self?.spellDetailed = spellDetailed
                     self?.spellLabel.text = spellDetailed.name
                     self?.spellDesc.text = spellDetailed.desc[0]
@@ -55,9 +87,11 @@ class SpellDetailViewController: UIViewController {
                     self?.spellDuration.text = spellDetailed.duration
                     self?.spellLevel.text = String(spellDetailed.level)
                     self?.spellCT.text = spellDetailed.casting_time
+                    
+                    self?.querySpellbook()
                 }
                 
-
+                
             } catch {
                 debugPrint(error)
                 print("❌ Error parsing JSON: \(error.localizedDescription)")
@@ -68,9 +102,27 @@ class SpellDetailViewController: UIViewController {
         
         // Do any additional setup after loading the view.
         
-        //spellLabel.text = spellDetailed?.name
-        //spellDesc.text = spellDetailed?.desc[0]
         
     }
-        
+    
+    // Fetch specified spellbook
+    private func querySpellbook() {
+        let query = Spellbooks.query()
+            .where("username" == User.current?.username , "name" == User.current?.currentSpellbookName)
+
+        query.find { [weak self] result in
+            switch result {
+            case .success(let spellbook):
+                self?.spellBook = spellbook[0]
+            case .failure(let error):
+                print("Error querying database: "+error.localizedDescription)
+            }
+        }
+    }
+    private func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Please try again...")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
 }
