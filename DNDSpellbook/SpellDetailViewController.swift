59e7c6e5768e2b124f6432d2e3f7f06d85290ee3
Tristan Williams <tristanwilliamsrc@gmail.com>
2023-05-07 21:50:21 -0700

    Spell View Work in progress

--- DNDSpellbook/SpellDetailViewController.swift
+++ DNDSpellbook/SpellDetailViewController.swift
@@ -9,16 +9,53 @@ import UIKit
 
 class SpellDetailViewController: UIViewController {
     
+    @IBOutlet weak var spellLabel: UILabel!
+    @IBOutlet weak var spellDesc: UILabel!
+    
     var spell: Spell!
+    var spellDetailed: SpellDetail?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Gets index to use for spell url from ClassSpellViewController
-        // ex) "https://www.dnd5eapi.co/api/spells/(index)"
-        print(spell.index)
+        
+        let url = URL(string: "https://www.dnd5eapi.co/api/spells/" + spell.index)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
 
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+
+                let response = try decoder.decode(SpellDetailResponse.self, from: data)
+
+                let spellDetailed = response.result
+
+                DispatchQueue.main.async {
+
+                    self?.spellDetailed = spellDetailed
+                }
+
+
+            } catch {
+                debugPrint(error)
+                print("❌ Error parsing JSON: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+        
         // Do any additional setup after loading the view.
+        
+        
+        spellLabel.text = spellDetailed?.name
+        spellDesc.text = spellDetailed?.desc[0]
     }
-
-
 }
